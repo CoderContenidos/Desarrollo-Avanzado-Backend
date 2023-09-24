5ec7fd85b6efc164d6e50831a799c6942e6ec69f
Joaquin Cejas <joaquin.cejas.2020@gmail.com>
2023-09-24 12:23:34 -0300

    Finish preentrega 1

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
 import productRouter from './routes/productRouter.js';
+import cartRouter from './routes/cartRouter.js';
 import viewsRouter from './routes/viewsRouter.js';
 import __dirname from './utils/constantsUtil.js';
 
@@ -18,6 +19,7 @@ app.use(express.static('public'));
 
 //Routers
 app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
 app.use('/products', viewsRouter);
 
 const PORT = 8080;
